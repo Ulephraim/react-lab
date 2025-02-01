8389d3f0bb68b70fc4f1baee6571dfb72664c675
Ulephraim <iphraimunlimited@gmail.com>
2025-02-01 21:03:59 +0100

    setup counter

--- my-react-app/src/projects/Hooks/useStateHook.tsx
+++ my-react-app/src/projects/Hooks/useStateHook.tsx
@@ -9,14 +9,19 @@ const UseStateHook = () => {
   const [message, setMessage] = useState<string>('');
 
   return (
-    <div>
-      <button onClick={() => setMessage('Hello There')}>Say Hello</button>
-      <button onClick={() => setMessage('GoodBye')}>Say Goodbye</button>
+    <>
+      <div>
+        <button onClick={() => setMessage('Hello There')}>Say Hello</button>
+        <button onClick={() => setMessage('GoodBye')}>Say Goodbye</button>
 
-      <br />
-      <Card msg={message} />
-      <div>{message}</div>
-    </div>
+        <br />
+        <Card msg={message} />
+        <div>{message}</div>
+      </div>
+      <div>
+        <Counter />
+      </div>
+    </>
   );
 };
 
@@ -24,4 +29,15 @@ export default UseStateHook;
 
 const Card = ({ msg }: cardProps) => {
   return <div>{msg}</div>;
+};
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((state) => state + 1)}>Increment</button>
+    </div>
+  );
 };
