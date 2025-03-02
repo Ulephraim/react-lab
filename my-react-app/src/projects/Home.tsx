533a7622eaf1975bef4018c73388b6c7ce1dded8
Ulephraim <iphraimunlimited@gmail.com>
2025-03-02 15:11:31 +0100

    Implement Add Todo functionality

--- my-react-app/src/projects/Home.tsx
+++ my-react-app/src/projects/Home.tsx
@@ -4,13 +4,28 @@ import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
-    <div>
-      Welcome Home <br />
-      <Link to="/crud-home">CRUD</Link> <br />
-      <Link to="/donate">Donate</Link> <br />
-      <Link to="/payment">Payment</Link> <br />
-      <Link to="/mail">Mail</Link> <br />
-      <Link to="/grid">Grid</Link>
+    <div className="home-container">
+      <h1>Welcome Home</h1>
+      <div className="button-group">
+        <Link to="/crud-home" className="btn">
+          CRUD
+        </Link>
+        <Link to="/donate" className="btn">
+          Donate
+        </Link>
+        <Link to="/payment" className="btn">
+          Payment
+        </Link>
+        <Link to="/mail" className="btn">
+          Mail
+        </Link>
+        <Link to="/grid" className="btn">
+          Grid
+        </Link>
+        <Link to="/query" className="btn">
+          React Query
+        </Link>
+      </div>
     </div>
   );
 }
--- my-react-app/src/projects/QUERY/query.tsx
+++ my-react-app/src/projects/QUERY/query.tsx
@@ -1,7 +1,57 @@
 /** @format */
 
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { addTodo, fetchTodos } from '../..';
+import { useState } from 'react';
+
 function Query() {
-  return <div>QUERY</div>;
+  const queryClient = useQueryClient();
+
+  const [title, setTitle] = useState<string>('');
+  const { data: todos, isLoading } = useQuery({
+    queryFn: () => fetchTodos(),
+    queryKey: ['todos'],
+  });
+
+  const { mutateAsync: addTodoMutation } = useMutation({
+    mutationFn: addTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] });
+    },
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+        <button
+          onClick={async () => {
+            try {
+              await addTodoMutation({ title });
+              setTitle('');
+            } catch (e) {
+              console.error(e);
+            }
+          }}
+        >
+          Add Todo
+        </button>
+      </div>
+      <div>
+        {todos?.map((todo) => {
+          return <div key={todo.id}>{todo.title}</div>;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Query;
